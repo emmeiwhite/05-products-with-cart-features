9b98a899e4eca93cb9a05dbb01eb5f2a15098587
emmeiwhite <emmeiwhite@gmail.com>
2025-05-05 15:29:23 +0530

    shopping-cart-2.5 we are creating a common index.js file within controllers to import/export with one line and avoid long file paths every time

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,4 +3,11 @@ export { default as registerController } from './auth/registerController.js'
 /** The above line is equivalent to :
  * import registerController from './auth/registerController.js';
  * export registerController
+ * // NOTE:
+ *  This is a named export now & not a default export now.
+ *
+ * So, in our routes folder's index.js file we have to import it as {registerController}
+ *
+ * routes/index.js
+ * import { registerController } from './../controllers/index.js'
  */

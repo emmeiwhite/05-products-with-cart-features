cae2284ef90c9c35be740938afdf0d33f66c2351
emmeiwhite <emmeiwhite@gmail.com>
2025-05-05 15:26:23 +0530

    shopping-cart-2.4 importing registerController in Routes folder for business logic

---
+++ server/controllers/index.js
@@ -0,0 +1,6 @@
+export { default as registerController } from './auth/registerController.js'
+
+/** The above line is equivalent to :
+ * import registerController from './auth/registerController.js';
+ * export registerController
+ */
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import registerController from '../controllers/registerController'
+
+import { registerController } from './../controllers/index.js'
 
 const router = express.Router()
 
